ebcd73b7d3109643bc1911640d9337efb9043993
Andreas Eulenwald <andreas@eulenwald.de>
2023-10-30 14:51:35 +0100

    Add controls

---
+++ controls001.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hallo, hier startet unser kleines Programm!")
+
+	foo()
+	fmt.Println("Foo ist beendet, unser kleines Programm geht evt. weiter!")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+
+}
+
+func foo() {
+	fmt.Println("Hier sind wir in foo()")
+}
+
+func bar() {
+	fmt.Println("Hier sind wir in bar()")
+}
+
+// Sequences
+// Loops / iterieren
+// conditionals
